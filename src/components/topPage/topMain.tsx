3f39a7c725d01cfada214fac98db1b92c4fd3e78
jackkitte <jackkitte735@gmail.com>
2020-07-08 21:20:39 +0900

    React Hooksを使った関数コンポーネントでのstate管理

--- src/components/topPage/topMain.tsx
+++ src/components/topPage/topMain.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Pager from '@material-ui/core/Paper';
 import SerachIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
 import pencil from '../../assets/images/pencil.png';
 
 const useStyle = makeStyles(() =>
@@ -24,12 +25,17 @@ const useStyle = makeStyles(() =>
 
 const TopMain: FC = () => {
   const classes = useStyle();
+  const [keyword, setKeyword] = useState("");
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(event.target.value);
+  }
   return (
     <div className={classes.background}>
       <Pager className={classes.pager}>
         <IconButton type="submit">
           <SerachIcon />
         </IconButton>
+        <InputBase onChange={handleChange} placeholder="無料素材を検索" />
       </Pager>
     </div>
 
